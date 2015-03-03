f8f96120b83419706c0a2cf3a4e2ba7275bfeec1
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-04 00:20:17 +0100

    Bump version to 0.0.1

--- src/SolutionInfo.fs
+++ src/SolutionInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyCompanyAttribute("Yaaf.Xmpp.ServiceDiscovery")>]
-[<assembly: AssemblyProductAttribute("Yaaf.Xmpp.ServiceDiscovery")>]
-[<assembly: AssemblyCopyrightAttribute("Yaaf.Xmpp.ServiceDiscovery Copyright © Matthias Dittrich 2015")>]
+[<assembly: AssemblyCompanyAttribute("Yaaf.Xmpp.VCard")>]
+[<assembly: AssemblyProductAttribute("Yaaf.Xmpp.VCard")>]
+[<assembly: AssemblyCopyrightAttribute("Yaaf.Xmpp.VCard Copyright © Matthias Dittrich 2015")>]
 [<assembly: AssemblyVersionAttribute("0.0.1")>]
 [<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.0.1")>]
